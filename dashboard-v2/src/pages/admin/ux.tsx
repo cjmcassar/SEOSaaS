13d0f616d69a3c2dcc1a71d0b7ff7593d484ab38
Christopher-John Cassar <46441540+cjmcassar@users.noreply.github.com>
2023-10-23 23:59:20 -0700

    Delete dashboard-v2 directory

--- dashboard-v2/src/pages/admin/ux.tsx
+++
@@ -1,5 +0,0 @@
-import { Content } from "../../components/Content";
-
-export default function UIUXPage() {
-  return <Content title="UI/UX" />;
-}
--- dashboard-v2/src/pages/admin/videos.tsx
+++
@@ -1,5 +0,0 @@
-import { Content } from "../../components/Content";
-
-export default function VideosPage() {
-  return <Content title="Videos" />;
-}
