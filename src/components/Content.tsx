2d2ccf87f4ef19b70bdeaafe6c95aacac78b452a
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-10-31 11:38:24 -0700

    feat: Added ProjectCard and KeyWordGenerator comps

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -11,8 +11,8 @@ const ProjectCard = () => {
 						<span className="text-sm">December 10, 2020</span>
 					</div>
 					<div className="mb-4 mt-5 text-center">
-						<p className="text-base font-bold opacity-70">Web Designing</p>
-						<p className="mt-2 text-sm opacity-70">Prototyping</p>
+						<p className="text-base font-bold opacity-70">Landing Page</p>
+						<p className="mt-2 text-sm opacity-70">Keywords List</p>
 					</div>
 					<div>
 						<p className="m-0 text-sm font-bold">Progress</p>
@@ -59,47 +59,34 @@ const ProjectCard = () => {
 	);
 };
 
-const ClientMessages = () => {
+const KeyWordGenerator = () => {
 	return (
-		<div className="rounded-3xl bg-gray-800 px-6 pt-6">
+		<div className="rounded-3xl bg-gray-800 px-6 pt-6 ">
 			<div className="flex pb-6 text-2xl font-bold text-white">
-				<p>Client Messages</p>
+				<p>Generate Keywords</p>
 			</div>
 			<div>
-				{/* Repeat this block for each message */}
-				<div className="flex w-full border-t border-gray-700 p-4 hover:bg-gray-700 2xl:items-start rounded-lg">
-					<img
-						src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-						alt="profile image"
-						className="h-10 w-10 rounded-full object-cover"
+				<form>
+					<label
+						className="mb-2 block text-sm font-bold text-gray-400"
+						htmlFor="business"
+					>
+						What does your business do?
+					</label>
+					<textarea
+						className=" w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-400 shadow focus:outline-none"
+						id="business"
+						placeholder="Your answer"
 					/>
-					<div className="w-full pl-4">
-						<div className="flex w-full items-center justify-between">
-							<div className="font-medium text-white">Stephanie</div>
-							<div className="flex h-7 w-7 cursor-pointer items-center justify-center">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="20"
-									height="20"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									className="text-white"
-								>
-									<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-								</svg>
-							</div>
-						</div>
-						<p className="my-2 text-sm text-gray-400">
-							I got your first assignment. It was quite good. 🥳 We can continue
-							with the next assignment.
-						</p>
-						<p className="text-right text-sm text-gray-400">Dec, 12</p>
+					<div className="mt-4 flex justify-center">
+						<button
+							className="mb-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+							type="button"
+						>
+							Submit
+						</button>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
@@ -116,15 +103,15 @@ export function Content(props: ContentProps) {
 				<div className="flex flex-wrap items-center justify-between pb-8">
 					<div className="flex flex-wrap text-white">
 						<div className="pr-10">
-							<div className="text-2xl font-bold">45</div>
-							<div className="">In Progress</div>
+							<div className="text-2xl font-bold">1200</div>
+							<div className="">Keywords Generated</div>
 						</div>
 						<div className="pr-10">
-							<div className="text-2xl font-bold">24</div>
-							<div className="">Upcoming</div>
+							<div className="text-2xl font-bold">10</div>
+							<div className="">Sessions</div>
 						</div>
 						<div>
-							<div className="text-2xl font-bold">62</div>
+							<div className="text-2xl font-bold">3</div>
 							<div className="">Total Projects</div>
 						</div>
 					</div>
@@ -136,7 +123,7 @@ export function Content(props: ContentProps) {
 				</div>
 			</div>
 			<div className="mt-8 w-full lg:mt-0 lg:w-4/12 lg:pl-4">
-				<ClientMessages />
+				<KeyWordGenerator />
 			</div>
 		</div>
 	);
