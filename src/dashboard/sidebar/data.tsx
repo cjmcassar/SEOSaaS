6cd638d5512af677819e5685035250e042dcd7a3
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-10-31 10:41:56 -0700

    feat: removed unused items

--- src/dashboard/sidebar/data.tsx
+++ src/dashboard/sidebar/data.tsx
@@ -1,39 +1,17 @@
-import { DocIcon } from "./icons/DocIcon";
 import { HomeIcon } from "./icons/HomeIcon";
-import { StatusIcon } from "./icons/StatusIcon";
-import { CreditIcon } from "./icons/CreditIcon";
-import { ArchiveIcon } from "./icons/ArchiveIcon";
+
 import { SettingsIcon } from "./icons/SettingsIcon";
 
 export const data = [
-  {
-    title: "Home",
-    icon: <HomeIcon />,
-    link: "/",
-  },
-  {
-    title: "Status",
-    icon: <StatusIcon />,
-    link: "/admin/status",
-  },
-  {
-    title: "Archives",
-    icon: <ArchiveIcon />,
-    link: "/admin/archives",
-  },
-  {
-    title: "Credits",
-    icon: <CreditIcon />,
-    link: "/admin/credits",
-  },
-  {
-    title: "Settings",
-    icon: <SettingsIcon />,
-    link: "/admin/settings",
-  },
-  {
-    title: "Documentation",
-    icon: <DocIcon />,
-    link: "/admin/documentation",
-  },
+	{
+		title: "Home",
+		icon: <HomeIcon />,
+		link: "/",
+	},
+
+	{
+		title: "Settings",
+		icon: <SettingsIcon />,
+		link: "/admin/settings",
+	},
 ];
