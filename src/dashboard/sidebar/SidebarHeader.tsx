66b72f4ad9fbf2d25d4fba13f623f2f8d786cd6b
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-10-31 10:37:39 -0700

    feat: Update SidebarHeader to display KeyFind logo text

--- src/dashboard/sidebar/SidebarHeader.tsx
+++ src/dashboard/sidebar/SidebarHeader.tsx
@@ -1,7 +1,7 @@
 export function SidebarHeader() {
-  return (
-    <div className="sticky top-0 z-10 mb-6 flex items-center justify-center bg-gray-900 pb-6 pt-3">
-      <img src="/images/2.png" width={80} height={90} alt="Enoch Ndika" />
-    </div>
-  );
+	return (
+		<div className="sticky top-0 z-10 mt-2 flex items-center justify-center bg-gray-900 p-4">
+			<span className="text-xl font-bold text-white">KeyFind</span>
+		</div>
+	);
 }
