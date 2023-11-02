8d7ecb6a52ceec5956010a9fe4b3cf485d9c74ef
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-02 16:46:19 -0700

    fix: removed unused code

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -40,38 +40,6 @@ const ProjectCard = ({ data }: ProjectCardProps) => {
 							Full CSV file
 						</a>
 					</div>
-					<div className="relative flex justify-between pt-4">
-						<div className="flex items-center">
-							<img
-								className="h-5 w-5 overflow-hidden rounded-full object-cover"
-								src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-								alt="participant11"
-							/>
-							<img
-								className="h-5 w-5 overflow-hidden rounded-full object-cover"
-								src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
-								alt="participant12"
-							/>
-							<button className="ml-3 flex h-5 w-5 items-center justify-center rounded-full border-none bg-white p-0">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="12"
-									height="12"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="3"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								>
-									<path d="M12 5v14M5 12h14" />
-								</svg>
-							</button>
-						</div>
-						<div className="flex shrink-0 rounded-lg px-4 py-2 text-sm font-bold text-purple-700">
-							2 Days Left
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
