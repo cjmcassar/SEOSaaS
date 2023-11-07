81a88a2fa5548625022b7104acc74f1f19fd5402
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-06 17:12:08 -0800

    feat: added formquestion data file

---
+++ src/components/data/FormQuestions.ts
@@ -0,0 +1,10 @@
+export const questionSteps = [
+	{
+		id: 1,
+		question: "What does your business do?",
+		name: "business",
+		placeholder: "Your answer",
+		validation: { required: true },
+	},
+	// Add more steps as needed
+];
