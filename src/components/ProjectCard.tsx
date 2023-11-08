f06ce27580201638ad28e4cea6fad1d15501d63b
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-07 16:55:02 -0800

    feat: Centered download button and added func

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -27,14 +27,25 @@ export const ProjectCard = ({ data }: ProjectCardProps) => {
 						</p>
 					</div>
 					<div>
-						<p className="m-0 text-sm font-bold">Download</p>
-						<a
-							href="/path/to/csv/file.csv"
-							download
-							className="mx-0 my-2 text-purple-700 underline"
-						>
-							Full CSV file
-						</a>
+						<p className="m-0 text-center text-sm font-bold">Download CSV</p>
+						<div className="flex justify-center">
+							<button
+								onClick={async () => {
+									const response = await fetch("/path/to/csv/file.csv");
+									const blob = await response.blob();
+									const url = window.URL.createObjectURL(blob);
+									const a = document.createElement("a");
+									a.href = url;
+									a.download = "file.csv";
+									document.body.appendChild(a);
+									a.click();
+									a.remove();
+								}}
+								className="mx-0 my-2 rounded bg-purple-700 px-4 py-2 text-white"
+							>
+								Full CSV file
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
