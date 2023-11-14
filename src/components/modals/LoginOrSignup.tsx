debb10a50395284a17ed4ff9d2d1327b5b98ce9e
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-14 01:58:11 -0800

    feat: added prevent default

--- src/components/modals/LoginOrSignup.tsx
+++ src/components/modals/LoginOrSignup.tsx
@@ -11,7 +11,8 @@ const LoginOrSignup: React.FC<Props> = ({ onClose }) => {
 	const [password, setPassword] = useState("");
 	const [loading, setLoading] = useState(false);
 
-	const handleSignUp = async () => {
+	const handleSignUp = async (event: { preventDefault: () => void }) => {
+		event.preventDefault();
 		await supabase.auth.signUp({
 			email,
 			password,
@@ -22,16 +23,12 @@ const LoginOrSignup: React.FC<Props> = ({ onClose }) => {
 		router.reload();
 	};
 
-	const handleSignIn = async () => {
+	const handleSignIn = async (event: { preventDefault: () => void }) => {
+		event.preventDefault();
 		await supabase.auth.signInWithPassword({
 			email,
 			password,
 		});
-		router.reload();
-	};
-
-	const handleSignOut = async () => {
-		await supabase.auth.signOut();
 		router.reload();
 	};
 
@@ -56,11 +53,19 @@ const LoginOrSignup: React.FC<Props> = ({ onClose }) => {
 					/>
 					<div className="flex flex-col items-center space-y-2">
 						<button
+							onClick={handleSignIn}
+							disabled={loading}
+							className="mb-2 w-1/2 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700 focus:outline-none"
+						>
+							{loading ? "Loading..." : "Log In"}
+						</button>
+						<hr className="w-full border-t border-gray-300" />
+						<button
 							type="submit"
 							disabled={loading}
 							className="mb-2 w-1/2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
 						>
-							{loading ? "Loading..." : "Submit"}
+							{loading ? "Loading..." : "Sign Up"}
 						</button>
 						<button
 							onClick={onClose}
