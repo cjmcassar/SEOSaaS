e68e8fc945c2d3ad5b9834e5bc6717c50cd4fc8b
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-15 22:33:27 -0800

    feat: created profile setup questions

---
+++ src/components/data/ProfileSetupQuestions.ts
@@ -0,0 +1,63 @@
+export const profileSetupQuestions = [
+	{
+		id: 1,
+		question: "What industry does your business serve?",
+		name: "industry",
+		placeholder: "e.g., Software, Healthcare, Retail, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 2,
+		question: "What is your business model?",
+		name: "businessModel",
+		placeholder: "e.g., SaaS, Agency, Ecommerce, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 3,
+		question: "What are your most profitable products or services?",
+		name: "profitableProducts",
+		placeholder: "e.g., Product A, Service B, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 4,
+		question: "Who is your target audience or ideal customer?",
+		name: "targetAudience",
+		placeholder: "e.g., Small business owners, Teenagers, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 5,
+		question:
+			"What content are your competitors creating that is also relevant to your business?",
+		name: "competitorsContent",
+		placeholder:
+			"e.g., Blog posts about industry trends, Product reviews, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 6,
+		question: "What is your business’s Unique Value Proposition?",
+		name: "valueProposition",
+		placeholder:
+			"e.g., Highest quality in the market, Best customer service, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 7,
+		question: "What are your primary business objectives?",
+		name: "businessObjectives",
+		placeholder:
+			"e.g., Increase market share, Improve customer satisfaction, etc.",
+		validation: { required: true },
+	},
+	{
+		id: 8,
+		question:
+			"What are the key features and benefits of your products/services?",
+		name: "keyFeatures",
+		placeholder: "e.g., Feature A (Benefit A), Feature B (Benefit B), etc.",
+		validation: { required: true },
+	},
+];
