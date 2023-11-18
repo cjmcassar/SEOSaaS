624c0e650f5e35d81d4e9df9df0c5255510120ad
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-17 18:05:58 -0800

    feat: Updated user fetching logic in app.tsx to correctly use Supabase's auth.session()

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,24 +17,34 @@ type User = {
 function MyApp({ Component, pageProps }: AppProps) {
 	const [user, setUser] = useState<User | null>(null);
 	// Todo: make this use state a cookie instead of a state later. Just doing this atm to test route
-	const [isProfileSetupComplete, setProfileSetupComplete] = useState(false);
+	const [isProfileSetupComplete, setProfileSetupComplete] = useState();
 	const router = useRouter();
 
 	useEffect(() => {
 		const fetchUser = async () => {
 			console.log("Fetching user...");
 			const { data, error } = await supabase.auth.getUser();
-			if (error) {
+			if (error || !data.user) {
 				console.error("Error fetching user:", error);
 			} else {
-				console.log("User fetched successfully:", data);
+				console.log("User fetched successfully:", user);
 				setUser({
-					id: data.user.id,
-					email: data.user.email || null,
+					id: data.user?.id,
+					email: data.user?.email || null,
 				});
-				const isSetupComplete = false;
-				// Todo: setup database so this information is there
-				setProfileSetupComplete(isSetupComplete);
+
+				const { data: profileData, error: profileError } = await supabase
+					.from("profiles")
+					.select("created_profile")
+					.eq("user_id", user?.id)
+					.single();
+
+				if (profileError) {
+					console.error("Error fetching profile:", profileError);
+				} else {
+					const isSetupComplete = profileData?.created_profile;
+					setProfileSetupComplete(isSetupComplete);
+				}
 			}
 		};
 
