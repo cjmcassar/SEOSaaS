a803218b4fc2db06b15e4477df3d3714ff0b36ce
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-21 20:07:29 -0800

    feat: added initial keyword generator from gpt

--- src/components/KeywordGenerator.tsx
+++ src/components/KeywordGenerator.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { keyWordQuestionSteps } from "./data/KeywordQuestions";
 import { supabase } from "@/utils/supabaseClient";
+
+import { ProfileInfoContext } from "@/contexts/ProfileInfoContext";
+import { UserContext } from "@/contexts/UserContext";
 
 type FormValues = {
   // Define your form fields here. For example:
@@ -13,8 +16,12 @@ type FormValues = {
 };
 
 export const KeyWordGenerator = () => {
+  const profileInfo = useContext(ProfileInfoContext);
+  const user = useContext(UserContext);
   const { register, handleSubmit } = useForm<FormValues>();
   const [step, setStep] = useState(1);
+
+  console.log("Profile Info:", profileInfo);
 
   const onSubmit: SubmitHandler<FormValues> = async data => {
     console.log(data);
@@ -25,15 +32,62 @@ export const KeyWordGenerator = () => {
         page_type: data.contentType,
         page_focus: data.contentFocus,
         audience_faqs: data.audienceFAQs,
+        user_id: user?.user?.id,
       };
 
-      const { error } = await supabase
+      const { data: keywordGenPrompt, error } = await supabase
         .from("keyword_gen_prompt")
-        .insert([mappedData]);
+        .insert([mappedData])
+        .select();
 
       if (error) {
         console.error("Error inserting data: ", error);
       } else {
+        // Implementing the open ai prompt module getGptResponse here
+        console.log("keyword prompt data from supabase:", keywordGenPrompt);
+        const keywordGenPromptId = keywordGenPrompt[0].id;
+
+        const promptData = {
+          page_type: mappedData.page_type,
+          page_focus: mappedData.page_focus,
+          audience_faqs: mappedData.audience_faqs,
+        };
+
+        const prompt = `SEO marketer with profile ${JSON.stringify(
+          profileInfo,
+        )} is seeking to generate at least 35 keywords for their company. The provided data for this task is ${JSON.stringify(
+          promptData,
+        )}. Please provide the SEO keyword suggestions as a continuous text and not as a list or numbered items. Only return the keywords, no other descriptions or assistant text is necessary.`;
+        console.log("the updated prompt:", prompt);
+
+        const gptResponse = await fetch("/api/gptResponse", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt }),
+        });
+
+        const data = await gptResponse.json();
+        console.log("GPT Says:", data);
+
+        const { data: gptResponseData, error: gptError } = await supabase
+          .from("gpt_keyword_results")
+          .insert([
+            {
+              keyword_generator_id: keywordGenPromptId,
+              gpt_response: data.choices?.[0].message.content,
+              user_id: user?.user?.id,
+            },
+          ])
+          .select();
+
+        console.log("GPT Response Data:", gptResponseData);
+
+        if (gptError) {
+          console.error("Error inserting GPT response: ", gptError);
+        }
+
         restartProcess();
       }
     }
