d1af5dbf7adddf4b483a7c924a34a5505bfd0f34
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-26 20:28:30 -0800

    refactor: changed names

--- src/utils/googleadsapi.ts
+++
@@ -1,9 +0,0 @@
-import { GoogleAdsApi } from "google-ads-api";
-
-const client = new GoogleAdsApi({
-  client_id: process.env.GOOGLE_CLIENT_ID ?? "",
-  client_secret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-  developer_token: process.env.GOOGLE_DEVELOPER_TOKEN ?? "",
-});
-
-export default client;
