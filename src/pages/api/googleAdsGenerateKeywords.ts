36fa2bdb93e08b64f3f528db969857fc8831dd82
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-27 20:58:24 -0800

    feat: Implemented keyword extraction from request body

--- src/pages/api/googleAdsGenerateKeywords.ts
+++ src/pages/api/googleAdsGenerateKeywords.ts
@@ -1,17 +1,17 @@
 import { getAuthHeader } from "@/utils/dataForSeoAuth";
+import { parse } from "json2csv";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const post_array = [];
-  post_array.push({
-    location_name: "United States",
-    keywords: ["phone", "cellphone"],
-  });
+  const { keywords } = req.body;
 
-  //todo: get data from the gpt response table in supabase
+  const post_array = keywords.map((keyword: { gpt_response: string }) => ({
+    location_name: "United States",
+    keywords: keyword.gpt_response.split(", "), // assuming keywords are comma-separated
+  }));
 
   let taskId;
 
@@ -28,7 +28,10 @@ export default async function handler(
       },
     );
     if (!postResponse.ok) {
-      throw new Error(`HTTP error! status: ${postResponse.status}`);
+      res
+        .status(400)
+        .json({ error: `HTTP error! status: ${postResponse.status}` });
+      return;
     }
     const postResult = await postResponse.json();
 
@@ -46,9 +49,13 @@ export default async function handler(
   }
 
   // Wait for a certain amount of time before retrieving the postResults
-  await new Promise(resolve => setTimeout(resolve, 7000)); // Wait for 5 seconds
+  await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 5 seconds
 
-  try {
+  async function fetchTask(taskId: string, attempt: number = 1): Promise<any> {
+    if (attempt > 10) {
+      throw new Error("Maximum number of attempts reached");
+    }
+
     const getResponse = await fetch(
       "https://api.dataforseo.com/v3/keywords_data/google_ads/keywords_for_keywords/task_get/" +
         taskId,
@@ -60,12 +67,36 @@ export default async function handler(
         },
       },
     );
+
     if (!getResponse.ok) {
       throw new Error(`HTTP error! status: ${getResponse.status}`);
     }
+
     const getResult = await getResponse.json();
-    console.log(getResult);
-    res.status(200).json({ getResult });
+
+    console.log("results response:", getResult);
+
+    if (getResult.tasks?.[0].status_code !== 20000) {
+      // Wait for 2 seconds before trying again
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      return fetchTask(taskId, attempt + 1);
+    } else {
+      return getResult;
+    }
+  }
+
+  // Use the function
+  try {
+    const getResult = await fetchTask(taskId);
+
+    // Convert JSON to CSV
+
+    const resultArray = getResult.tasks[0].result;
+
+    const csv = parse(resultArray);
+
+    res.setHeader("Content-Type", "text/csv");
+    res.status(200).send(csv);
     //todo: sift through the data, convert to a CSV, and store in supabase
   } catch (error) {
     const err = error as Error;
