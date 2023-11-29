32975d7df7fc6943f502f303448dde13e401ea60
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-11-29 01:01:03 -0800

    feat: added in sb event listener to see when new data is added

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,69 +1,100 @@
+import { useCallback, useContext, useEffect, useState } from "react";
 import { KeyWordGenerator } from "./KeywordGenerator";
 import { ProjectCard } from "./ProjectCard";
+import { UserContext } from "@/contexts/UserContext";
+import { supabase } from "@/utils/supabaseClient";
 
 interface ContentProps {
-	title: string;
+  title: string;
 }
 
 interface ProjectData {
-	title: string;
-	date: string;
-	keywords: string;
+  id: string;
+  title: string;
+  created_at: string;
+  project_type: string;
+  gpt_keyword_sample: string;
+  csv_file_path: string;
+  amount_of_keywords: number;
+}
+
+export interface ProjectCardProps {
+  data: ProjectData;
 }
 
 export function Content(props: ContentProps) {
-	const projectData1: ProjectData = {
-		title: "Landing Page",
-		date: "December 10, 2020",
-		keywords:
-			"Fashion, Apparel, E-commerce, Online Shopping, SaaS, Marketplace, Clothing, Retail, B2C, Consumer",
-	};
+  const [projects, setProjects] = useState<ProjectData[] | null>(null);
+  const user = useContext(UserContext);
+  console.log("user:", user);
 
-	const projectData2: ProjectData = {
-		title: "Marketplace",
-		date: "December 2, 2022",
-		keywords: "E-commerce, Online Shopping, Marketplace, Retail, B2C",
-	};
+  const fetchProjects = useCallback(async () => {
+    if (user) {
+      const { data: projectsData, error } = await supabase
+        .from("projects")
+        .select("*")
+        .eq("user_id", user?.user?.id);
 
-	const projectData3: ProjectData = {
-		title: "About Us",
-		date: "December 1, 2023",
-		keywords:
-			"clothing store, about us page, fashion, apparel, e-commerce, online shopping, retail, B2C",
-	};
+      if (error) console.log("Error fetching project data: ", error);
+      else {
+        setProjects(projectsData);
+        console.log("projects data:", projectsData);
+      }
+    }
+  }, [user]);
 
-	return (
-		<div className="flex flex-wrap">
-			<div className="w-full rounded-3xl bg-gray-800 p-6 lg:w-8/12">
-				<div className="mb-8 flex items-center justify-between text-white">
-					<p className="text-2xl font-bold">{props.title}</p>
-					<p className="">December, 12</p>
-				</div>
-				<div className="flex flex-wrap items-center justify-between pb-8">
-					<div className="flex flex-wrap text-white">
-						<div className="pr-10">
-							<div className="text-2xl font-bold">1200</div>
-							<div className="">Keywords Generated</div>
-						</div>
-						<div className="pr-10">
-							<div className="text-2xl font-bold">10</div>
-							<div className="">Sessions</div>
-						</div>
-						<div>
-							<div className="text-2xl font-bold">3</div>
-							<div className="">Total Keyword List</div>
-						</div>
-					</div>
-				</div>
-				<div className="flex flex-wrap">
-					<ProjectCard data={projectData1} />
-					<ProjectCard data={projectData2} />
-					<ProjectCard data={projectData3} />
-				</div>
-			</div>
-			<div className="mt-8 w-full lg:mt-0 lg:w-4/12 lg:pl-4">
-				<KeyWordGenerator />
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    fetchProjects();
+
+    const subscription = supabase
+      .channel("room1")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "projects" },
+        payload => {
+          console.log("Change received!", payload);
+        },
+      )
+      .subscribe();
+    return () => {
+      supabase.removeChannel(subscription);
+    };
+  }, [fetchProjects]);
+
+  if (!projects) return <div>Loading...</div>;
+  //TODO: update this to grab the data from supabase or something
+
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full rounded-3xl bg-gray-800 p-6 lg:w-8/12">
+        <div className="mb-8 flex items-center justify-center text-white">
+          <p className="text-2xl font-bold">{props.title}</p>
+        </div>
+        <div className="flex flex-wrap items-center justify-between pb-8">
+          <div className="flex flex-wrap text-white">
+            <div className="pr-10">
+              <div className="text-2xl font-bold">
+                {projects?.reduce(
+                  (total, project) => total + project.amount_of_keywords,
+                  0,
+                )}
+              </div>
+              <div className="">Keywords Generated</div>
+            </div>
+            <div className="pr-10">
+              <div className="text-2xl font-bold">{projects?.length}</div>
+              <div className="">SEO Sessions</div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap">
+          {projects?.map(project => (
+            <ProjectCard key={project.id} data={project} />
+          ))}
+        </div>
+      </div>
+      <div className="mt-8 w-full lg:mt-0 lg:w-4/12 lg:pl-4">
+        <KeyWordGenerator />
+      </div>
+    </div>
+  );
 }
