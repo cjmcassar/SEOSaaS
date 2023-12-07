945c4b3db5786f50bb29d2347ecab5cc9fc8a49c
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-12-07 10:19:44 -0800

    feat: added name question

--- src/components/data/KeywordQuestions.ts
+++ src/components/data/KeywordQuestions.ts
@@ -1,32 +1,39 @@
 export const keyWordQuestionSteps = [
   {
     id: 1,
-    question:
-      "Are the keywords for your homepage, landing page, product page, or blog post?",
-    name: "contentType",
-    type: "dropdown", // Specify that this is a dropdown type question
-    options: ["Homepage", "Landing Page", "Product Page", "Blog Post"],
-    placeholder: "Select an option",
+    question: "What is the name of your project?",
+    name: "contentName",
+    placeholder: "Example: SuperSaaS",
     validation: { required: true },
   },
   {
     id: 2,
     question:
-      "What is the focus of the page / keyword list? Be as specific or general as you like!",
-    name: "contentFocus",
-    placeholder: "Your answer",
+      "Are the keywords for your homepage, landing page, product page, or blog post?",
+    name: "contentType",
+    type: "dropdown", // Specify that this is a dropdown type question
+    options: ["Homepage", "Landing Page", "Product Page", "Blog Post"],
+    placeholder: "Select a page type",
     validation: { required: true },
   },
   {
     id: 3,
     question:
-      "What frequently asked questions from your audience do you want to answer in the content?",
-    name: "audienceFAQs",
-    placeholder: "Your answer",
+      "What is the focus of the page / keyword list? Be as specific or general as you like!",
+    name: "contentFocus",
+    placeholder: "Example: SEO optimization for SaaS products",
     validation: { required: true },
   },
   {
     id: 4,
+    question:
+      "What frequently asked questions from your audience do you want to answer in the content?",
+    name: "audienceFAQs",
+    placeholder: "Example: How does SEO optimization work?",
+    validation: { required: true },
+  },
+  {
+    id: 5,
     question:
       "That's it! Submit your answers to generate your project's SEO keywords!",
     name: "finalStep",
